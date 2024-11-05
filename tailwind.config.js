935e4f48563bedbbc0062eda86ca82da2d2e7c1d
Bilal Asif <sba97492956@gmail.com>
2024-11-05 21:37:42 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'archivo': ['"Archivo", sans-serif'],
+        'biryani': ['"Biryani", sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
